dc2929bbe4ba4bd37b2234bfd41db7618e6ea9b7
Fafy <fabianrojas2801@gmail.com>
2019-07-02 19:37:43 -0300

    Config variables

--- lua/ps_bodygrouppicker/sh_config.lua
+++ lua/ps_bodygrouppicker/sh_config.lua
@@ -1,4 +1,26 @@
 local CONFIG = {}
 
+-- [[ Derma ]] --
+
+-- The color of the bar at the top of the window
+CONFIG.DermaBar = Color(25, 25, 25)
+-- The background of the window
+CONFIG.DermaBackground = Color(50, 50, 50)
+-- The background of the model rectangle
+CONFIG.ModelBackground = Color(0, 0, 255)
+
+-- [[ Server ]] --
+
+CONFIG.UserGroups = {}
+-- User groups that can modify bodygroups (put false to prevent, can also add/remove your own usergroups)
+
+CONFIG.UserGroups['user'] = true
+CONFIG.UserGroups['member'] = true
+CONFIG.UserGroups['supporter'] = true
+CONFIG.UserGroups['vip'] = true
+CONFIG.UserGroups['trialadmin'] = true
+CONFIG.UserGroups['admin'] = true
+CONFIG.UserGroups['senioradmin'] = true
+CONFIG.UserGroups['superadmin'] = true
 
 return CONFIG
